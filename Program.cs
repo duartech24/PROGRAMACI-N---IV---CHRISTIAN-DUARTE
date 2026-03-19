1340ab8c91962c2161802c87f06d80b7dc1d4510
24CDL01-ChristianDuarte <160366029+duartech24@users.noreply.github.com>
2026-03-19 12:12:37 -0600

    Add files via upload
    Ejercicio en clases

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    //Verificar si un numero es positivo
+    //o negativo
+    private static void Main(string[] args)
+    {
+        //Entradas
+        Console.WriteLine("===NUMEROS POSITIVOS Y NEGRATIVOS");
+        int num;
+        Console.Write("Dame un numero entero: ");
+        num=Convert.ToInt32(Console.ReadLine());
+        //Procesos
+        //Salidas
+        if (num == 0)
+        {
+            Console.WriteLine("Es un número POSITIVO");
+        }
+        else
+        {
+            Console.WriteLine("Es un numero NEGATIVO");
+        }
+    }
+}
